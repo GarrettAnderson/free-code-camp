b0c26217fde5fa2527b47cca51eb36bbc026e4ae
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-09-05 17:02:35 -0400

    removed comments from counting-cards js and added switch statement for multiple cases to count the card value by 1 or subtract by 1 or subtract by 0 given the card count. added function for the recursive example to keep track of challenges. reference free code camp for more info

---
+++ javascript/replace-loops-with-recursion.js
@@ -0,0 +1,19 @@
+//Write a recursive function, sum(arr, n),
+//that returns the sum of the first n elements of an array arr. 
+
+function sum(arr, n) {
+    // Only change code below this line
+    if (n <= 0) {
+      return 0
+    } else {
+      return sum(arr, n-1) + arr[n-1]
+    }
+    // Only change code above this line
+  }
+
+  // Test Cases
+  /*
+sum([1], 0) should equal 0.
+sum([2, 3, 4], 1) should equal 2.
+sum([2, 3, 4, 5], 3) should equal 9.
+  */
