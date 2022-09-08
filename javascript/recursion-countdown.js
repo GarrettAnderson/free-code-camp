1cdd6113be0aa4054ac39dccf9ce07f16c22aace
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-09-08 16:49:19 -0400

    added notes for recursion countdown function

--- javascript/recursion-countdown.js
+++ javascript/recursion-countdown.js
@@ -12,6 +12,18 @@ function countup(n) {
   The value [1, 2, 3, 4, 5] will be displayed in the console.
   
   
-  
+
 
   */
+
+
+  function countdown(n){
+    if (n < 1) {
+      return []
+    } else {
+      const countArray = countArray(n - 1) 
+      countArray.unshift(n)
+      console.log(countArray)
+      return countArray
+    }
+  }
