0b2b53c246038032ca8c457297678384f58463b6
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-09-15 19:42:37 -0400

    added some code for the getters and setters for notes

--- javascript/object-getters-setters.js
+++ javascript/object-getters-setters.js
@@ -31,3 +31,25 @@ Notice the syntax used to invoke the getter and setter. They do not even look li
 
 Note: It is convention to precede the name of a private variable with an underscore (_). However, the practice itself does not make a variable private.
 */
+
+// Only change code below this line
+class Thermostat {
+    constructor(temperature) {
+      this.temperature = temperature
+    }
+  
+    get temperature() {
+      return this.temperature
+    }
+  
+    set temperature(updatedTemperature) {
+      this._author = updatedTemperature
+    }
+  
+  }
+  // Only change code above this line
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature; // 24.44 in Celsius
+  thermos.temperature = 26;
+  temp = thermos.temperature; // 26 in Celsius
