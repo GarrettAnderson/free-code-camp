975cc3f369898d3e39906a0b79b1a6149364c7fc
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-10-02 16:27:34 -0400

    updated the reversedStr variable to have a spread operator for the reverse method

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -1,6 +1,6 @@
 function palindrome(str) {
     const splitStr = str.split("")
-    const reversedStr = splitStr.reverse()
+    const reversedStr = [...splitStr].reverse()
   
     for (var i = 0; i < splitStr.length; i++) {
       for (var j = 0; j < reversedStr.length; j++) {
@@ -11,4 +11,7 @@ function palindrome(str) {
     }
     return false
   }
+  
+  console.log(palindrome("not a palindrome"))
+  console.log(palindrome("My age is 0, 0 si ega ym."))
   
