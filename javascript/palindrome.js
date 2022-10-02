a243dd02beb0a1d7c31b4534eb2df9f04e6ddfb9
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-10-02 16:48:38 -0400

    made sure that the input string is set to lowercase

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -1,17 +1,29 @@
 function palindrome(str) {
-    const splitStr = str.split("")
-    const reversedStr = [...splitStr].reverse()
-  
-    for (var i = 0; i < splitStr.length; i++) {
-      for (var j = 0; j < reversedStr.length; j++) {
-        if (splitStr[i] === reversedStr[j]) {
-            return true
-        } 
-      }
-    }
+    
+  const newStr = str.replace(/[^A-Z0-9]/ig, "").toLowerCase()
+  const splitStr = newStr.split("")
+  const reversedStrArr = [...splitStr].reverse()
+  const reversedStr = reversedStrArr.join("")
+
+  if (newStr === reversedStr) {
+    return true
+  } else {
     return false
   }
   
-  console.log(palindrome("not a palindrome"))
+  // const splitStr = str.split("")
+    // const reversedStr = [...splitStr].reverse()
+  
+    // for (var i = 0; i < splitStr.length; i++) {
+    //   for (var j = 0; j < reversedStr.length; j++) {
+    //     if (splitStr[i] === reversedStr[j]) {
+    //         return true
+    //     } 
+    //   }
+    // }
+    // return false
+  }
+  
+  console.log(palindrome("A man, a plan, a canal. Panama"))
   console.log(palindrome("My age is 0, 0 si ega ym."))
   
