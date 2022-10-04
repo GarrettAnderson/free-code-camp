5d444ab616306e2480ce9d0230805d2f63eea854
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2022-10-04 19:59:41 -0400

    added beginning of the caesars cipher algorithm

---
+++ javascript/caesars-cipher.js
@@ -0,0 +1,11 @@
+function rot13(str) {
+
+    // split input string into an array
+  
+    // iterate the array
+  
+      // for each letter, traverse the alphabet thirteen letters 
+    return str;
+  }
+  
+  rot13("SERR PBQR PNZC");
